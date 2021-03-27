82081c0dd658390a5c1f017182be1b5dd8a34a07
Hibana <pagliaccizeus@gmail.com>
2021-03-27 15:44:32 +0100

    second version of tic tac toe and add version on jquery :D :D

--- main.js
+++ main.js
@@ -3,9 +3,79 @@ let countClick = 0;
 let countWins_0 = 0;
 let countWins_X = 0;
 
+//creating a playing field on pure JavaScript
+// function createGame() {
+//     //create game arr
+//     for (let i =0;i<rowArr.length;i++) {
+//         rowArr[i] = new Array(3);
+//     }
+//     //creating a playing field
+//     let html = "";
+//     for(let i=0;i<rowArr.length;i++) {
+//         for (let j=0;j<rowArr[i].length;j++) {
+//             rowArr[i][j] = "";
+//             html += `<button value="${i +" "+ j}"  class="btn_click">${rowArr[i][j]}</button>`
+//             document.querySelector("#block_game").innerHTML = html;
+//         }
+//     }
+//     countClick = 0
+// }
+
+//main logic on pure JavaScript
+// function game () {
+//     document.querySelector("#block_game").addEventListener("click",function(e) {
+//         if (e.target.classList.contains("btn_click")) {
+//             if (countClick % 2 === 0) {
+//                 playerTurn("0",e);
+//             } else if (countClick % 2 !== 0) {
+//                 playerTurn("X",e)
+//             }
+//         } 
+//     })  
+// }
+
+//function of player turn on pure JavaScript
+// function playerTurn (val,e) {
+//     let position = e.target.value;
+//     let arr = position.split(" ");
+//     rowArr[arr[0]][arr[1]] = val
+
+//     if (e.target.innerText === "") {
+//         countClick++;
+//         e.target.innerText = val;
+
+//         if (check(val) && val === "0") {
+//             createGame();
+//             document.querySelector("#wins_0").innerHTML = ++countWins_0;
+//         } else if (check(val) && val === "X") {
+//             createGame();
+//             document.querySelector("#wins_X").innerHTML = ++countWins_X;
+//         }
+//     } else {
+//         alert("you already input on button");
+//     }
+// }
 
 
-//creating a playing field
+//function of reset on pure JavaScript
+// function resetGame() {
+//     createGame();
+//     countClick = 0
+//     countWins_0 = 0;
+//     countWins_X = 0;
+//     document.querySelector("#wins_0").innerHTML = 0;
+//     document.querySelector("#wins_X").innerHTML = 0;
+// }
+
+
+//reset of game on pure JavaScript
+// document.querySelector("#btn_startNewGame").onclick = ()=> {
+//     resetGame();
+// };
+
+
+
+//creating a playing field on jquery
 function createGame() {
     //create game arr
     for (let i =0;i<rowArr.length;i++) {
@@ -17,59 +87,49 @@ function createGame() {
         for (let j=0;j<rowArr[i].length;j++) {
             rowArr[i][j] = "";
             html += `<button value="${i +" "+ j}"  class="btn_click">${rowArr[i][j]}</button>`
-            document.querySelector("#block_game").innerHTML = html;
+            $("#block_game").html(html);
+
         }
     }
-
     countClick = 0
 }
 
-
-//main logic
-function Game () {
-    document.querySelector("#block_game").addEventListener("click",function(e) {
+//main logic on pure jquery
+function game () {
+    $("#block_game").on("click",function(e) {
         if (e.target.classList.contains("btn_click")) {
             if (countClick % 2 === 0) {
-                    let position = e.target.value;
-                let arr = position.split(" ");
-                rowArr[arr[0]][arr[1]] = "0"
-
-                if (e.target.innerText === "") {
-                    countClick++;
-                    e.target.innerText = "0";
-
-                    if (check("0")) {
-                        createGame();
-                        document.querySelector("#wins_0").innerHTML = ++countWins_0;
-                    }
-                } else {
-                    alert("you already input on button");
-                }
-
-                
+                playerTurn("0",e);
             } else if (countClick % 2 !== 0) {
-                let position = e.target.value;
-                    let arr = position.split(" ");
-                    rowArr[arr[0]][arr[1]] = "X"
-                    console.log(rowArr)
-        
-                    if (e.target.innerText === "") {
-                        countClick++;
-                        e.target.innerText = "X";
-                    } else {
-                        alert("you already input on button");
-                    }
-
-                    if (check("X")) {
-                        createGame();
-                        document.querySelector("#wins_X").innerHTML = ++countWins_X;
-                    }
+                playerTurn("X",e)
             }
         } 
-    })  
+    })
 }
 
+//function of player turn on jquery
+function playerTurn (val,e) {
+    let position = e.target.value;
+    let arr = position.split(" ");
+    rowArr[arr[0]][arr[1]] = val
 
+    if (e.target.innerText === "") {
+        countClick++;
+        e.target.innerText = val;
+
+        if (check(val) && val === "0") {
+            createGame();
+            $("#wins_0").html(++countWins_0);
+        } else if (check(val) && val === "X") {
+            createGame();
+            $("#wins_X").html(++countWins_X)
+        }
+    } else {
+        alert("you already input on button");
+    }
+}
+
+//check the winner
 function check(val) {
     if ((rowArr[0][0] == val && rowArr[0][1] == val && rowArr[0][2] == val) 
     || (rowArr[1][0] == val && rowArr[1][1] == val && rowArr[1][2] == val) 
@@ -83,22 +143,26 @@ function check(val) {
     || (rowArr[0][2] == val && rowArr[1][1] == val && rowArr[2][0] == val)) {
         return true;
     } 
-}
+};
 
-
-
-
-document.querySelector("#btn_startNewGame").onclick = ()=> {
+//function of reset on jqeury
+function resetGame() {
     createGame();
     countClick = 0
     countWins_0 = 0;
     countWins_X = 0;
-    document.querySelector("#wins_0").innerHTML = 0;
-    document.querySelector("#wins_X").innerHTML = 0;
+    $("#wins_0").html(0);
+    $("#wins_X").html(0);
 }
 
+//reset of game on jquery
+$("#btn_startNewGame").on("click",()=> {
+    resetGame();
+})
+
+
 createGame();
-Game();
+game();
 
 
 
@@ -114,61 +178,5 @@ Game();
 
 
 
-
-
-
-
-//Rework Coming Soon:
-// function clickPlayer() {
-//     document.querySelector("#block_game").addEventListener("click",function(e) {
-//         if (e.target.classList.contains("btn_click")) {
-//             let position = e.target.value;
-//             // console.log(e.target.value);
-//             let arr = position.split(" ");
-//             // console.log(e.target.innerText);
-//             rowArr[arr[0]][arr[1]] = "0"
-//             // console.log(rowArr)
-//             // e.target.innerText === "" ? e.target.innerText = "0" : alert("you already input on button"); 
-
-//             if (e.target.innerText === "") {
-//                 e.target.innerText = "0";
-//                 countClick++;
-//             } else {
-//                 alert("you already input on button");
-//             }
-//             // console.log(rowArr);
-//         } 
-        
-//         // console.log(countClick);
-//     })  
-// }
-
-
-// function clickBot() {
-//     document.querySelector("#block_game").addEventListener("click",function(e) {
-//         if (e.target.classList.contains("btn_click")) {
-//             let position = e.target.value;
-//             // console.log(e.target.value);
-//             let arr = position.split(" ");
-//             // console.log(e.target.innerText);
-//             rowArr[arr[0]][arr[1]] = "Х"
-//             console.log(rowArr)
-//             e.target.innerText === "" ? e.target.innerText = "Х" : alert("you already input on button"); 
-
-//             if (e.target.innerText === "") {
-//                 e.target.innerText = "X";
-//                 countClick++;
-//             } else {
-//                 alert("you already input on button");
-//             }
-
-//             // console.log(rowArr);
-//         }
-//         // console.log(countClick);
-//         // countClick++;
-//         // console.log(countClick);
-//     })
-   
-// }
 
 
